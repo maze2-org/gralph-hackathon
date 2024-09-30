b00b8f706ab26dec0ce1b5b2e5334f219410467d
Aram <aramsuqiasyan20@gmail.com>
2024-09-30 18:48:06 +0400

    feat: change GUI

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
+// @ts-ignore
+import {ruruHTML} from "ruru/server";
 import dotenv from "dotenv";
 import express from 'express';
-import {graphqlHTTP} from 'express-graphql';
+
 import schema from './graphql/schema';
-import rootValue from './graphql/resolvers';
+import rootValue from "./graphql/resolvers";
+import {createHandler} from 'graphql-http/lib/use/express';
 import {connectToMongo} from "./db/connect";
 
 dotenv.config();
@@ -12,11 +15,15 @@ const app = express();
 // Connect to MongoDB
 connectToMongo().catch()
 
-// GraphQL middleware for handling queries
-app.use('/graphql', graphqlHTTP({
-  schema,
-  rootValue,
-  graphiql: true,
-}));
+app.get('/', (req, res) => {
+  res.redirect(301, '/graphql');
+});
+
+app.get("/graphql", (_req, res) => {
+  res.type("html")
+  res.end(ruruHTML({endpoint: "/graphql"}))
+})
+
+app.all('/graphql', createHandler({schema, rootValue}));
 
 export default app;
